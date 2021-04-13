2be384110d41be9ad142c952e0c756ad53ca63b5
Vikvass <76476616+Vikvass@users.noreply.github.com>
2021-04-13 14:07:19 +0300

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -1,36 +1,39 @@
 from tkinter import *
 
-
 window = Tk()
 
-
 def clear():
-    global fon, btnnow, btnins, description, uslovie, uslovie1
+    global fon, btnnow, btnins, description, uslovie1
     fon.destroy()
     btnnow.destroy()
     btnins.destroy()
     description.destroy()
-    uslovie.destroy()
-
 
 def instruksia():
-    global description, btnnachal
+    global description, btnnachal,uslovie
     clear()
     description = Label(window, text="Инструкция", font=("Calibri bold", 30,), background="#0210A1",fg="white")
     description.place(x='100', y="75")
     btnnachal = Button(window, bg ="#42AAFF",text="Вернуться в начало", command=begin, activebackground='#18FFA2')
     btnnachal.place(x=15, y=485, width=100, height=100)  # кнопка, которая ведет в начало игры после инструкции
+    uslovie = Label(
+        window,
+        text="Как же все таки играть в ТеТрИс!?\nТвоя задача опускать в ряд фигуры,"
+             "\nТак чтобы весь ряд был закрыт\nКогда весь ряд будет закрыт"
+             "\nУ тебя исчезнет этот ряд\nИ за это тебе дадут очки!"
+             "\nИгра закончится если ряды не будут исчезать,"
+             "\nИ все поле будет занято:((",font=("Calibri", 20,), background="#18FFFD",justify="left")
+    uslovie.place(x='30', y="170")
 
 
 def vtoroepole():
-    global btnnachall
+    global btnnachall,uslovie
     clear()
     btnnachall = Button(window, bg="#42AAFF", text="Вернуться в начало", command=begin, activebackground='#18FFA2')
     btnnachall.place(x=15, y=485, width=100, height=100)
 
-
 def begin():
-    global fon, btnnow, description, uslovie, btnins
+    global fon, btnnow, description, btnins
     window.geometry('600x600')
     fon = Frame(window, bg='#18FFFD')
     fon.place(x=0, y=0, relwidth=1, relheight=1)
@@ -46,14 +49,6 @@ def begin():
     )
     btnins.place(x=485, y=485, width=100, height=100)
     window.title("Добро пожаловать в ТеТрИс")
-    uslovie = Label(
-        window,
-        text="Как же все таки играть в ТеТрИс!?\nТвоя задача опускать в ряд фигуры,"
-             "\nТак чтобы весь ряд был закрыт\nКогда весь ряд будет закрыт"
-             "\nУ тебя исчезнет этот ряд\nИ за это тебе дадут очки!"
-             "\nИгра закончится если ряды не будут исчезать,"
-             "\nИ все поле будет занято:((",font=("Calibri", 20,), background="#18FFFD",justify="left")
-    uslovie.place(x='30', y="170")
     description = Label(
         window,
         text="Начни игру в ТеТрИс!",
@@ -62,8 +57,29 @@ def begin():
         fg="white")
     description.place(x='100', y="75")
     fon = Frame(window, bg='#18FFA2')
+    c = Canvas(window, width=200, height=200, bg='#18FFFD')
+    c.pack()
+
+    c.create_rectangle(60, 60, 40, 130,
+                       fill='yellow',
+                       outline='green',
+                       width=3,
+                       activedash=(5, 4))
+    c.create_rectangle(80, 110, 60, 130,
+                       fill='yellow',
+                       outline='green',
+                       width=3,
+                       activedash=(5, 4))
+    c.create_rectangle(80, 110, 40, 130,
+                       fill='yellow',
+                       outline='green',
+                       width=3,
+                       activedash=(5, 4))
+    c.create_line(60, 110, 60, 130,
+                  width=3)
+    c.create_line(40, 87, 60, 87,
+                  width=3)
 
 
 begin()
-
 window.mainloop()
