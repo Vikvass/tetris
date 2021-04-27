d00c0cef5cc5286bab23e81ba3dc623da70cd12c
Vikvass <76476616+Vikvass@users.noreply.github.com>
2021-04-27 14:05:10 +0300

    Add files via upload

--- figura.py
+++ figura.py
@@ -1,26 +1,26 @@
 from tkinter import *
 window = Tk()
-c = Canvas(window, width=200, height=200, bg='#18FFFD')
+c = Canvas(window, width=600, height=600, bg='#18FFFD')
 c.pack()
 
 def create_ugolok(x,y,side):
     figure=[
-     c.create_rectangle(x, y, x-side, y+side,
-                       fill = 'yellow',
-                       outline = 'green',
+     c.create_rectangle(x, y, x - side, y + side,
+                       fill='yellow',
+                       outline='green',
                        width = 3,
                        activedash = (5, 4)),
-    c.create_rectangle(x, y+side, x, y-side,
+    c.create_rectangle(x, y, x - side, y-side,
                        fill='yellow',
                        outline='green',
                        width=3,
                        activedash=(5, 4)),
-    c.create_rectangle(x, y+2*side, x-side, y+side,
+    c.create_rectangle(x, y - side, x-side, y- 2 *side,
                        fill='yellow',
                        outline='green',
                        width=3,
                        activedash=(5, 4)),
-    c.create_rectangle(x+side, y, x+side, y-side,
+    c.create_rectangle(x, y, x+side, y + side,
                        fill='yellow',
                        outline='green',
                        width=3,
@@ -28,5 +28,8 @@ def create_ugolok(x,y,side):
     ]
     return figure
 
+create_ugolok(400,300,100)
+c.pack()
+window.mainloop()
 
 
